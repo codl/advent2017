bb611f56a36a8dc936c7ff205f866d8f76ffcbea
codl <codl@codl.fr>
2017-12-14 22:53:00 +0100

    14-2

--- 14.py
+++ 14.py
@@ -1,26 +1,67 @@
 knothash = __import__("10-2", fromlist=('densehash',)).densehash
+import random
 
 
 def knothashb(_in):
     bytes_ = knothash(_in)
     out = list()
-    for byte in bytes_:
-        for i in range(7, -1, -1):
-            out.append(bool(byte & 2**i))
+    for idx in range(len(bytes_)):
+        for jdx in range(8):
+            if(bytes_[idx] & 2**(7-jdx)):
+                out.append(idx*8 + jdx)
     return tuple(out)
 
 
+def neighbours(coord):
+    x, y = coord
+    return {
+            (x-1, y),
+            (x+1, y),
+            (x, y-1),
+            (x, y+1)
+        }
+
+
 def run(_in):
-    rows = map(lambda i: knothashb('{}-{}'.format(_in, i)), range(128))
-    rows = tuple(rows)
+    squares = set()
+    groups = set()
 
-    count = 0
-    for row in rows:
-        for square in row:
-            if square:
-                count += 1
+    def _debug():
+        CHARS="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        groupchars = dict()
+        for row in range(128):
+            for column in range(128):
+                square = (column, row)
+                if square in squares:
+                    char = '#'
+                    for group in groups:
+                        if square in group:
+                            if char != "#":
+                                raise Exception()
+                            if group not in groupchars.keys():
+                                groupchars[group] = random.choice(CHARS)
+                            char = groupchars[group]
+                            break
+                    print(char, end='')
+                else:
+                    print('.', end='')
+            print('')
+        print('')
 
-    return count
+    for row in range(128):
+        for column in knothashb('{}-{}'.format(_in, row)):
+            squares.add((column, row))
+
+    for square in squares:
+        group = set((square,))
+        group |= squares & neighbours(square)
+        for other in set(groups):
+            if other & group:
+                group |= other
+                groups.remove(other)
+        groups.add(frozenset(group))
+
+    return len(squares), len(groups)
 
 
 if __name__ == '__main__':
